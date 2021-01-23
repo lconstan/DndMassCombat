e7f2db17529fb9dd2ae50caeac95bd7702973930
nu-ke <lconstan793336@gmail.com>
2021-01-23 19:36:40 +0100

    Add damage bonus + change display name

--- DndMassCombat/DndMassCombat/Models/Simulation/GroupViewModel.cs
+++ DndMassCombat/DndMassCombat/Models/Simulation/GroupViewModel.cs
@@ -1,11 +1,23 @@
-﻿namespace DndMassCombat.Models.Simulation
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DndMassCombat.Models.Simulation
 {
     public class GroupViewModel
     {
         public string Name { get; set; }
+        
+        [Display(Name = "Hit point")]
         public int HitPoint { get; set; }
+        
+        [Display(Name = "Armor class")]
         public int ArmorClass { get; set; }
+        
+        [Display(Name = "Hit bonus")]
         public int HitBonus { get; set; }
         public Damage Damage { get; set; }
+        
+        [Display(Name = "Damage bonus")]
+        
+        public int DamageBonus { get; set; }
     }
 }
