0b5611d1830915162ed21d53b0715addbd8b383e
nu-ke <lconstan793336@gmail.com>
2021-02-06 15:32:19 +0100

    Compute unitCount from hitpoint

--- DndMassCombat/DndMassCombat/wwwroot/js/site.js
+++ DndMassCombat/DndMassCombat/wwwroot/js/site.js
@@ -17,3 +17,32 @@ function setTrueAndSubmit(id) {
         $form.submit();
     }
 }
+
+// UnitCount = Group hit point / Unit hit point
+function assignUnitCount(id){
+    var groupHitpointString = document.getElementById(id).value;
+    
+    if (!isInt(groupHitpointString))
+        return;
+    
+    var groupNumberIdentifier = id.split("_");
+    var groupNumber = groupNumberIdentifier[0] === "Group1" ? 1 : 2;
+    var unitHitPointFieldId = "UnitDescription"+groupNumber+"_HitPoint";
+    var unitHitpointString = document.getElementById(unitHitPointFieldId).value;
+        
+    if (!isInt(unitHitpointString))
+        return;
+    
+    var groupHitpoint = parseInt(groupHitpointString);
+    var unitHitpoint = parseInt(unitHitpointString);
+    var unitCount = groupHitpoint / unitHitpoint;
+    
+    if (unitCount !== 0)
+        document.getElementById(groupNumberIdentifier[0]+"_UnitCount").value = unitCount;
+}
+
+function isInt(value) {
+    return !isNaN(value) &&
+        parseInt(Number(value)) == value &&
+        !isNaN(parseInt(value, 10));
+}
